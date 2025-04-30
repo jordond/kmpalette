6ccc30b3e2cb2a8f99ca467b705d08a32b38a655
Amit Kumar <mr.doc10jl96@gmail.com>
2025-04-30 17:54:53 +0530

    Fix CI workflows

--- .github/actions/calculate-tag/extract_tag.sh
+++ .github/actions/calculate-tag/extract_tag.sh
@@ -4,9 +4,11 @@ github_ref=$1
 draft_tag=$2
 
 if [[ $github_ref == refs/tags/v* ]]; then
-tag=${github_ref/refs\/tags\//}
-echo "VERSION=$tag" >> "$GITHUB_OUTPUT"
+    tag=${github_ref/refs\/tags\//}
+    tag="${tag:1}"
+    echo "VERSION=$tag" >> "$GITHUB_OUTPUT"
 else
-tag=$draft_tag
-echo "VERSION=$tag" >> "$GITHUB_OUTPUT"
+    tag=$draft_tag
+    tag="${tag:1}"
+    echo "VERSION=$tag" >> "$GITHUB_OUTPUT"
 fi
