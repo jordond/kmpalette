179f805921c0e751272be807070ef0b33aaca611
Jordon de Hoog <jordon.dehoog@gmail.com>
2026-02-06 13:45:51 -0500

    Add android specific loading

---
+++ kmpalette-core/src/androidMain/kotlin/com/kmpalette/extensions/resource/DrawableDominantColorState.kt
@@ -0,0 +1,48 @@
+package com.kmpalette.extensions.resource
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import com.kmpalette.DominantColorState
+import com.kmpalette.loader.rememberDrawableLoader
+import com.kmpalette.palette.graphics.Palette
+import com.kmpalette.rememberDominantColorState
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.compose.resources.DrawableResource
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * Wrapper around [rememberDominantColorState] that uses [com.kmpalette.loader.DrawableLoader] to load the image.
+ *
+ * @see rememberDominantColorState
+ * @param[defaultColor] The default color, which will be used if [Palette.generate] fails.
+ * @param[defaultOnColor] The default color to use _on_ [defaultColor].
+ * @param[cacheSize] The size of the LruCache used to store recent results. Pass `0` to disable.
+ * @param[coroutineContext] The [CoroutineContext] used to launch the coroutine.
+ * @param[context] The [Context] to use for loading drawables.
+ * @param[isSwatchValid] A lambda which allows filtering of the calculated [Palette.Swatch].
+ * @param[builder] A lambda which allows customization of the [Palette.Builder] used to generate
+ * the [Palette].
+ * @return A [DominantColorState] which can be used to generate a dominant color
+ * from a [DrawableResource].
+ */
+@Composable
+public fun rememberDrawableDominantColorState(
+    defaultColor: Color,
+    defaultOnColor: Color,
+    cacheSize: Int = DominantColorState.DEFAULT_CACHE_SIZE,
+    coroutineContext: CoroutineContext = Dispatchers.Default,
+    context: Context = LocalContext.current,
+    isSwatchValid: (Palette.Swatch) -> Boolean = { true },
+    builder: Palette.Builder.() -> Unit = {},
+): DominantColorState<Int> = rememberDominantColorState(
+    loader = rememberDrawableLoader(context = context),
+    defaultColor = defaultColor,
+    defaultOnColor = defaultOnColor,
+    cacheSize = cacheSize,
+    coroutineContext = coroutineContext,
+    isSwatchValid = isSwatchValid,
+    builder = builder,
+)
+
